6aba98cb335cad876c4fe09fcd79075d5313e623
Steve Lemke <steve.lemke@palm.com>
2012-08-29 13:12:51 -0700

    Create symlink for enyo version 1.0 (points to 0.10) in /usr/palm/frameworks/enyo. This matches what developers see on a 3.x device.
    Change-Id: Ie3c1d65e36cb3debfe67f05d9384cd5ff1496a66

--- build-luna-sysmgr.sh
+++ build-luna-sysmgr.sh
@@ -465,6 +465,9 @@ function build_enyo-1.0
     cd $BASE/enyo-1.0
     mkdir -p $ROOTFS/usr/palm/frameworks/enyo/0.10/framework
     cp -rf framework/* $ROOTFS/usr/palm/frameworks/enyo/0.10/framework
+    cd $ROOTFS/usr/palm/frameworks/enyo/
+    # add symlink for enyo version 1.0 (which was 0.10)
+    ln -sf -T 0.10 1.0
 }
 
 ###########################################
