c368c5d96aaf229a1f76fbc7c538ee14e26cb3ca
Eric Blade <eric.blade@palm.com>
2012-10-19 15:25:22 -0700

    Change universalsearchmgr copy path [OWEBOS-2659]
    Changed executable path for luna-universalsearchmgr package to LunaUniversalSearchMgr as changed in commit a9a7751d on luna-universalsearchmgr
    
    Open-webOS-DCO-1.0-Signed-off-by: Eric Blade <eric.blade@palm.com>
    
    Change-Id: I39c4b6f524f683d5aa170b69aa93ed6cbd9449a2

--- build-webos-desktop.sh
+++ build-webos-desktop.sh
@@ -1218,7 +1218,7 @@ function build_luna-universalsearchmgr
     make $JOBS
     make install
     # NOTE: Make binary findable in /usr/lib/luna so luna-universalsearchmgr can match the role file
-    cp -f $LUNA_STAGING/usr/sbin/luna-universalsearchmgr $ROOTFS/usr/lib/luna/LunaUniversalSearchMgr
+    cp -f $LUNA_STAGING/usr/sbin/LunaUniversalSearchMgr $ROOTFS/usr/lib/luna/LunaUniversalSearchMgr
     cp -f ../desktop-support/com.palm.universalsearch.json.pub $ROOTFS/usr/share/ls2/roles/pub/com.palm.universalsearch.json
     cp -f ../desktop-support/com.palm.universalsearch.json.prv $ROOTFS/usr/share/ls2/roles/prv/com.palm.universalsearch.json
     cp -f ../desktop-support/com.palm.universalsearch.service.pub $ROOTFS/usr/share/ls2/services/com.palm.universalsearch.service
