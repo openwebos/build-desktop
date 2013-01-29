6eac34085dfc37381646284de16f058fc9d9a208
Teppo Pennanen <teppo.pennanen@palm.com>
2013-01-29 14:49:59 +0200

    Export installed fonts path in run-luna-sysmgr.sh
    Fonts used by luna-sysmgr and applications (Prelude etc.) were installed
    into luna-desktop-binaries/rootfs/usr/share/fonts but luna-sysmgr looked
    for them in luna-desktop-binaries/staging/lib/fonts, where a different
    set of fonts was installed. Because of this wrong fonts were used
    everywhere.
    
    This change fixes OWEBOS-3092 where a non-Unicode font was used instead
    of a Unicode font.
    
    Open-webOS-DCO-1.0-Signed-off-by: Teppo Pennanen <teppo.pennanen@palm.com>
    
    Change-Id: I47da9ec7027d15016daaeab04d91dda11f353a86
    Reviewed-on: http://g2g.palm.com/1362
    Reviewed-by: Build Verification
    Reviewed-by: Teppo Pennanen <teppo.pennanen@palm.com>
    Tested-by: Teppo Pennanen <teppo.pennanen@palm.com>
    Reviewed-by: Jukka Honkela <jukka.honkela@palm.com>
    Reviewed-by: Roger Stringer <roger.stringer@palm.com>

--- run-luna-sysmgr.sh
+++ run-luna-sysmgr.sh
@@ -62,6 +62,7 @@ export LD_LIBRARY_PATH=${LIB_DIR}:${USRLIB_DIR}:${LD_LIBRARY_PATH}
 export PATH=${BIN_DIR}:${PATH}
 # Make Qt aware of this path (the qbsplugin is here)
 export QT_PLUGIN_PATH=${LUNA_STAGING}/plugins
+export QT_QPA_FONTDIR=${ROOTFS}/usr/share/fonts
 
 # To catch the CTRL-C
 trap terminateBrowserServer SIGINT
